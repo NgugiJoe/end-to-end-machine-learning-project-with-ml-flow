9aefbaa306d404e9daac656b79813d5eed25ae2f
Joe <ngugijoenyaga@gmail.com>
2023-08-03 08:24:51 -0700

    data ingestion added

---
+++ src/mlProject/entity/config_entity.py
@@ -0,0 +1,12 @@
+#step 4 
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+    
